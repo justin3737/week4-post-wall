d7849b87b6c21861ba409e0e0e67b1f025fb914f
Wu Justin <justin.wu@cathayholdings.com.tw>
2022-05-18 22:55:59 +0800

    修改posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,11 +1,12 @@
 const handleSuccess = require('../service/handleSuccess');
 const handleError = require('../service/handleError');
+const appError = require('../service/appError');
 
 const User = require('../models/users');
 const Post = require('../models/posts');
 
 const posts = {
-  async getPosts(req, res) {
+  async getPosts(req, res, next) {
     const timeSort = req.query.timeSort === "asc" ? "createAt" :  "-createAt";
     const q = req.query.q !== undefined ? { "content": new RegExp(req.query.q) } : {};
     const allPosts = await Post.find(q).populate({
@@ -14,23 +15,18 @@ const posts = {
     }).sort(timeSort);
     handleSuccess(res, allPosts);
   },
-  async createdPosts(req, res) {
-    try {
-      const { body } = req;
+  async createdPosts(req, res, next) {
+    const { body } = req;
 
-      if (body.content) {
-        const newPost = await Post.create({
-          user: body.user,
-          content: body.content,
-          image: body.image
-        })
-        handleSuccess(res, newPost);
-      } else {
-        handleError(res);
-      }
-    } catch (err){
-      handleError(res, err);
+    if (body.content === undefined || body.user === undefined) {
+      return next(appError(400, '你沒有填寫user或是content', next))
     }
+    const newPost = await Post.create({
+      user: body.user,
+      content: body.content,
+      image: body.image
+    })
+    handleSuccess(res, newPost);
   }
 }
 
