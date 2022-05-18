5c59522c6d1c91d8a40924b32dd155859602ed23
Wu Justin <justin.wu@cathayholdings.com.tw>
2022-05-18 23:36:57 +0800

    增加開發狀態與上線狀態的錯誤回饋

--- app.js
+++ app.js
@@ -34,17 +34,50 @@ app.use(function(req, res, next) {
   return next(appError(404, "找無此路由", next));
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// 開發環境錯誤
+const resErrorDev = (err ,res) => {
+  res.status(res.statusCode).json({
+    message: err.message,
+    error: err,
+    stack: err.stack
+  });
+ }
+
+// Prod環境錯誤
+// 自訂的錯誤
+const resErrorProd = (err ,res) => {
+  if (res.isOperational) {
+    res.status(res.statusCode).json({
+      message: err.message
+    });
+  } else {
+    // log 紀錄
+    console.error('出現重大錯誤', err);
+    // 送出罐頭預設訊息
+    res.status(500).json({
+      status: 'error',
+      message: '系統錯誤，請恰系統管理員'
+    });
+  }
+}
+
+// 錯誤處理 middleware
+app.use(function(err, res, req, next){
+  //dev
+  err.statusCode = err.statusCode || 500;
+  if (process.env.NODE_ENV === 'dev') {
+    return resErrorDev(err, res);
+  }
+
+  //production
+  if(err.name === 'ValidationError') {
+    err.message = "資料欄位未填寫正確，請重新輸入！"
+    err.isOperational = true;
+    return resErrorProd(err, res);
+  }
+  resErrorProd(err, res)
 });
-
 
 //未捕捉到的 catch
 process.on('unhandledRejection', (err, promise) => {
