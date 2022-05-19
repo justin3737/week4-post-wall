990e679c423bb4e599abdc8b4f2272239f201fe7
Wu Justin <justin.wu@cathayholdings.com.tw>
2022-05-19 11:46:31 +0800

    加入切換本地or遠端

--- connections/index.js
+++ connections/index.js
@@ -1,13 +1,12 @@
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
-dotenv.config({path: './config.env'});
 
-
-// 本地連線
-// mongoose.connect('mongodb://localhost:27017/posts')
-//   .then(() => {console.log('資料庫連線成功')})
-//   .catch(err => {console.log('資料庫連線失敗', err)});
-
+// 全域變數套件設定
+if (process.env.NODE_ENV === 'dev') {
+  dotenv.config({ path: "./local.env" })
+}else{
+  dotenv.config({ path: "./config.env" })
+}
 
 // 遠端連線
 const DB = process.env.DATABASE.replace(
